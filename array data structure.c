f5fb46b2dd1641c5b40ab04077f4b4edf75286bb
Siddharth Verma <88787415+vsiddharth365@users.noreply.github.com>
2022-10-10 22:57:36 +0530

    Update array data structure.c

--- array data structure.c
+++ array data structure.c
@@ -1,33 +1,26 @@
+// Author: Siddharth Verma
 #include <stdio.h>
-    void main()
+    int main()
     {
- 
         int i, j, a, n, number[30];
         printf("Enter the value of N \n");
         scanf("%d", &n);
- 
         printf("Enter the numbers \n");
         for (i = 0; i < n; ++i)
             scanf("%d", &number[i]);
- 
-        for (i = 0; i < n; ++i) 
+        for (i = 0; i < n; ++i)
         {
- 
             for (j = i + 1; j < n; ++j)
             {
- 
-                if (number[i] > number[j]) 
+                if (number[i] > number[j])
                 {
- 
- 
+                    number[i]=number[i]+number[j];
+                    number[j]=number[i]-number[j];
+                    number[i]=number[i]-number[j];
                 }
- 
             }
- 
         }
- 
         printf("The numbers arranged in ascending order are given below \n");
         for (i = 0; i < n; ++i)
-            printf("%d\n", number[i]);
- 
+            printf("%d ", number[i]);
     }
